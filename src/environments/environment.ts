4dbe926ffd4d5f59d13975c13abfd84cd852dc5d
carlos mur <cmurestudillos@gmail.com>
2020-10-07 08:50:35 +0200

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,3 @@
-import secureApiKey from '../config/firebasekey';
-import secureEnviroment from '../config/enviroment.js';
-
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: secureApiKey.configKey,
-    authDomain: secureEnviroment.ANGULAR_APP_AUTH_DOMAIN,
-    databaseURL: secureEnviroment.ANGULAR_APP_DATABASE_URL,
-    projectId: secureEnviroment.ANGULAR_APP_PROJECT_ID,
-    storageBucket: secureEnviroment.ANGULAR_APP_STORAGE_BUCKET,
-    messagingSenderId: secureEnviroment.ANGULAR_APP_MESSAGING_SENDER_ID,
-    appId: secureEnviroment.ANGULAR_APP_APP_ID,
-    measurementId: secureEnviroment.ANGULAR_APP_MEASUREMENT_ID
-  }
 };
